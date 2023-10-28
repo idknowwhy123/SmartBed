dc2d72b33645aa1e42d821281b5803e08511731d
Xchi <138505064+idknowwhy123@users.noreply.github.com>
2023-10-28 12:56:07 +0700

    Update Webcam.py

--- FaceDetetModel/Webcam.py
+++ FaceDetetModel/Webcam.py
@@ -1,14 +1,5 @@
-#from IPython import display
-
-#import ultralytics
-
 from ultralytics import YOLO
 import ultralytics
-
-#from IPython.display import display, Image
-
-#from ultralytics import YOLO
-ultralytics.checks()
 
 model = YOLO("best.pt")
 
